1ef7c7cedbf014ab374952ecce87d1f11ea41a6a
NAME SURNAME <austonmartin@gmail.com>
2025-05-02 17:43:39 +0800

    deploy adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7096408340945084"
+        crossorigin="anonymous"></script>
+      </head>
       <meta name="google-site-verification" content="zGviXckURsXcewn329Ky0DvyySEL3Yv_ZP6O4xpCUME" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
